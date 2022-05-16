3a186b947080252223e8dfe7e40489994a34a3ae
Aklima00 <aklima001begum@gmail.com>
2022-05-16 02:53:25 -0700

    my project

---
+++ footer.php
@@ -0,0 +1,12 @@
+
+<footer class="footer">
+	<hr/>
+&copy2022. Developed by Aklima ara begum
+<br/>
+Online Voting System.
+<br/>
+BCA 5th semester project 2022, BN College
+<br/>
+Contact: <a href="tel:+9136000">+91 9085++++++++</a> | Email: <a href="mailto:dncollege@mail.com">aklima01@uotlook.in</a>
+
+</footer>
---
+++ register/footer.php
@@ -0,0 +1,12 @@
+
+<div class="footer">
+	<hr/>
+&copy2022. Developed by Aklima ara begum 
+<br/>
+Online Voting System.
+<br/>
+BCA 5th semeter project 2022,B.N.College
+<br/>
+Contact: <a href="tel:+9136000">+91 9085++++++++</a> | Email: <a href="mailto:bncollege@gmail.com">aklima01@outlook.in</a>
+
+</div>
